6bdf8a17697da129c83b8d6df6fd49a2ff26b543
Marouf <maroufibrahimi7264@gmail.com>
2023-11-21 12:21:24 +0430

    adding todo Task Into List

--- src/App.js
+++ src/App.js
@@ -5,6 +5,22 @@ import "./App.css";
 
 const App = () => {
   const [isCompeleteColor, setIsCompeleteColor] = useState(false);
+  const [allTodos, setAllTodos] = useState([]);
+  const [newTitle, setNewTitle] = useState("");
+  const [newDescription, setNewDescription] = useState("");
+
+  const handleAddTodo = () => {
+    let newTodoItem = {
+      title: newTitle,
+      description: newDescription,
+    };
+
+    let updataTodoArr = [...allTodos];
+    updataTodoArr.push(newTodoItem);
+    setAllTodos(updataTodoArr);
+
+    console.log(updataTodoArr.title);
+  };
 
   return (
     <div className="App">
@@ -15,14 +31,28 @@ const App = () => {
         <div className="todo_input">
           <div className="todo_input_item">
             <label>Title</label>
-            <input type="text" placeholder="What's the task Title" />
+            <input
+              type="text"
+              value={newTitle}
+              onChange={(e) => setNewTitle(e.target.value)}
+              placeholder="What's the task Title"
+            />
           </div>
           <div className="todo_input_item">
             <label>Description</label>
-            <input type="text" placeholder="What's the task Description" />
+            <input
+              type="text"
+              value={newDescription}
+              onChange={(e) => setNewDescription(e.target.value)}
+              placeholder="What's the task Description"
+            />
           </div>
           <div className="todo_input_item">
-            <button type="button" className="primary_button">
+            <button
+              type="button"
+              onClick={handleAddTodo}
+              className="primary_button"
+            >
               Add
             </button>
           </div>
@@ -50,17 +80,19 @@ const App = () => {
 
         {/* all todos */}
         <div className="todo_lists">
-          <div className="todo_list_item">
-            <div>
-              <h3>Task</h3>
-              <p>Description</p>
-            </div>
+          {allTodos.map((todo, index) => (
+            <div className="todo_list_item" key={index}>
+              <div>
+                <h3>{todo.title}</h3>
+                <p>{todo.description}</p>
+              </div>
 
-            <div className="icons">
-              <MdDelete className="icon delete" />
-              <FaCheck className="icon check" />
+              <div className="icons">
+                <MdDelete className="icon delete" />
+                <FaCheck className="icon check" />
+              </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
