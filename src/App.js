067dbc6d70b92d48266bf0dc8706a49be7049c2b
Marouf <maroufibrahimi7264@gmail.com>
2023-11-24 15:58:54 +0430

    change 24 hours time to 12 hours

--- src/App.js
+++ src/App.js
@@ -68,6 +68,45 @@ const App = () => {
     let minute = now.getMinutes();
     let sec = now.getSeconds();
 
+    switch (hour) {
+      case 13:
+        hour = 1;
+        break;
+      case 14:
+        hour = 2;
+        break;
+      case 15:
+        hour = 3;
+        break;
+      case 16:
+        hour = 4;
+        break;
+      case 17:
+        hour = 5;
+        break;
+      case 18:
+        hour = 6;
+        break;
+      case 19:
+        hour = 7;
+        break;
+      case 20:
+        hour = 8;
+        break;
+      case 21:
+        hour = 9;
+        break;
+      case 22:
+        hour = 10;
+        break;
+      case 23:
+        hour = 11;
+        break;
+      case 24:
+        hour = 12;
+        break;
+    }
+
     let completedOn =
       date +
       "-" +
@@ -104,6 +143,7 @@ const App = () => {
   // edit the text
   const handleEditTodo = ({ id }) => {
     const findTodo = allTodos.find((todo) => todo.id === id);
+    const editTotos = [...allTodos];
     setEditTitle(findTodo);
     setEditDescription(findTodo);
   };
